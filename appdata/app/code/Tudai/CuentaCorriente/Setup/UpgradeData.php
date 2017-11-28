17bdd4363259028bc1638d49a86dc9b82f0ab925
Nico-Strella <strella.nicolas@gmail.com>
2017-11-28 11:53:50 -0300

    version del modulo 1.1.1 pequeño fix en la tabla

--- appdata/app/code/Tudai/CuentaCorriente/Setup/UpgradeData.php
+++ appdata/app/code/Tudai/CuentaCorriente/Setup/UpgradeData.php
@@ -42,7 +42,7 @@ class UpgradeData implements UpgradeDataInterface
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         if ($context->getVersion()
-            && version_compare($context->getVersion(), '1.1.0') < 0
+            && version_compare($context->getVersion(), '1.1.1') < 0
         ) {
             /**
              * @var CustomerSetup $customerSetup
